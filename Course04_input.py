48eea1870e27b7f23011619f0b69dbd9eeb583b3
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-16 19:51:58 +0300

    Add files via upload

---
+++ Course04_input.py
@@ -0,0 +1,6 @@
+name = input("Enter your name : ")  #veri almak için input yazıyoruz. 
+#ve bir değere aktariyoruz.
+print("Hello "+name+"!")
+
+age = input("How old are you : ")
+print("OW "+age+" is fantastic age.")
