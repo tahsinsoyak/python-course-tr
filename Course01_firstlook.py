2c5fcff15d54a20ad55ff980358f5830e7283f55
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-16 19:51:24 +0300

    Add files via upload

---
+++ Course01_firstlook.py
@@ -0,0 +1,13 @@
+print("Hello World") #ilk kodumuz
+
+character_age = "15"         #direk bir değişken belirleyip içine atadık
+character_name = "Suntheo"   #ikisinide string olarak atadik
+
+print("i am a "+character_name+" and i am "+character_age+" years old.")
+#yukaridaki string printine rakam koyamıyoruz. ***
+
+character_name = "Tahsin"
+print(character_name+" don't like swim.")
+
+character_age = 50    #int atadık.
+is_male = False   #boolean değer. 
