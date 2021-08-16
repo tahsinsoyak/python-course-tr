d113c4bf9c2e222025f012890cddb3c860dc730c
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-16 19:52:24 +0300

    Add files via upload

---
+++ Course05_calculator.py
@@ -0,0 +1,7 @@
+num1 = input("Enter a number : ") #aldigimiz veriler string
+num2 = input("Enter another number : ")
+# result = num1+num2 #bir string olarak birbirine ekledik.
+# result = int(num1)+int(num2)  #iki sayiyida int dönüştürdük
+#int kullandığımız için decimal kullanamayız
+result = float(num1)+float(num2)
+print(result)
