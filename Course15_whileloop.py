1deb65a5aa8de96927961441946bf3ae3c3b153b
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-08-18 17:43:30 +0300

    Add files via upload

---
+++ Course15_whileloop.py
@@ -0,0 +1,10 @@
+
+i = 1
+
+#bir boolean olarak değerlendirdigi durum doğru olduğu sürece
+#devam edecek while
+while i <= 10:
+    print(i)
+    i += 1 #eklemeyi bu şekilde kullandık [i = i+1]
+
+print("Done with loop")
