b3b36bc39aeac2a744779882b549c8299575839e
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-11-02 15:41:13 +0300

    Add files via upload

---
+++ Course25_modules.py
@@ -0,0 +1,11 @@
+import testmodule1
+# yarattığımız modülü kullanacağiz (kütüphane)
+
+print(testmodule1.roll_dice(15))
+# c# dll, kütüphaneler gibi
+
+# bütün önceden yazılan modüller
+# https://docs.python.org/3/py-modindex.html
+# Pycharm lib klasörü - build in
+# C:\Users\BMLAB\AppData\Local\Programs\Python\Python310\Lib
+
