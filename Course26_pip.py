0cb8664891a4be1e4ef612a31e677612e540ad57
tahsinsoyak <54424377+tahsinsoyak@users.noreply.github.com>
2021-11-02 15:41:28 +0300

    Add files via upload

---
+++ Course26_pip.py
@@ -0,0 +1,9 @@
+# pip package manager
+# "pip install python-docx" yaparak indiriyoruz
+#  Lib klasörünün içinde site-packages içine 
+# docx ve python_docx klasörü olarak gelir
+# kaldırmak için "pip uninstall python-docx"
+
+import docx
+
+docx.
